47ea2d91c684fa3162b9e4f3661de10e23505929
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-10 19:28:18 +0600

    added link with name & logo in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 import logo from "../../../public/assets/images/logo.jpg";
 const Navbar = () => {
   return (
     <div>
       <div className="navbar bg-base-100">
         <div className="navbar-start">
-          <div className="flex items-center gap-2">
-            <img className="w-14" src={logo} alt="" />
-            <h1 className="font-bold text-3xl">
-              <span className="text-purple-950"> Nex</span>
-              <span className="text-purple-700">Hire</span>
-            </h1>
+          <div>
+            <Link to={"/"} className="flex items-center gap-2">
+              <img className="w-14" src={logo} alt="" />
+              <h1 className="font-bold text-3xl">
+                <span className="text-purple-950"> Nex</span>
+                <span className="text-purple-700">Hire</span>
+              </h1>
+            </Link>
           </div>
         </div>
         <div className="navbar-center hidden lg:flex">
