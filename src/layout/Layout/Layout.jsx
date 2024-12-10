2f25c26eb80bc90912c80822b4ce97b96d6b78b6
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-10 19:21:19 +0600

    added navbar

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Layout = () => {
   return (
-    <div>
+    <div className="max-w-[1450px] mx-auto mt-4">
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
   );
