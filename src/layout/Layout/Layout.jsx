5ca72187cc219d3adfedd9a081ec4a80fec65403
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-10 22:21:47 +0600

    added footer

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
 
 const Layout = () => {
   return (
     <div className="max-w-[1450px] mx-auto mt-4">
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
