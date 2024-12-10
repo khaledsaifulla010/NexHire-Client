e126c6a6971a4467693f02ebf0865dc5c0b52cc7
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-10 19:02:24 +0600

    added layout component

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
+import Layout from "../layout/Layout/Layout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Layout></Layout>,
   },
 ]);
 
