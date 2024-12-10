9ae07cc13452120a26429f89b6fd21a3f55bff09
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-10 19:14:01 +0600

    added Home page

--- src/layout/Layout/Layout.jsx
+++ src/layout/Layout/Layout.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
-    return (
-      <div>
-        <h1>Layout</h1>
-      </div>
-    );
+  return (
+    <div>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Layout;
