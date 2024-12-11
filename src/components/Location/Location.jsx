3298ef791dcbf7ac407c09ab9c95241d8688d3c5
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-11 12:04:18 +0600

    added location selection option

--- src/components/Location/Location.jsx
+++ src/components/Location/Location.jsx
@@ -1,11 +1,33 @@
-import { FaLocationDot } from "react-icons/fa6";
-
+import { useEffect, useState } from "react";
+import { FaBriefcase, FaLocationDot } from "react-icons/fa6";
 const Location = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("../../../public/country/country.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setCountries(data);
+      });
+  }, []);
+
+  return (
+    <div className="relative">
+      <FaLocationDot className="text-xl absolute mt-7 ml-4 text-purple-800" />
+      <select className="select select-bordered w-[220px] text-center text-base text-slate-600 pl-4">
+        <option selected className="text-center text-lg text-slate-500">
+          Select Country
+        </option>
+        {countries.map((country) => (
+          <option
+            key={country.id}
+            className="text-center text-lg text-slate-500"
+          >
+            {country.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
 
 export default Location;
