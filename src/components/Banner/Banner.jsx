aed567ca13aeec8de384807d750c0c694d596b9e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-11 10:23:52 +0600

    added title & text in banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,23 +2,27 @@ import Lottie from "lottie-react";
 import bannerLottie from "../../../public/lottieFiles/bannerLottie.json";
 const Banner = () => {
   return (
-    <div className="mb-72 mt-12 border-2 rounded-xl">
-      <div className="hero bg-base-200 h-[600px]">
+    <div className="mb-72 mt-12 bg-purple-50">
+      <div className="hero h-[480px]">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="flex items-center">
-            <div className="w-[50%]">
-              <h1 className="text-5xl font-bold">
-                Your Next Opportunity Awaits!
+          <div className="flex items-center ">
+            <div className="w-[60%]">
+              <h1 className="text-5xl font-bold text-purple-950">
+                Your Next <span className="text-purple-600">Opportunity</span>{" "}
+                Awaits!
               </h1>
-              <p className="py-6">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
+              <p className="py-6 text-justify text-xl font-semibold text-slate-700">
+                Discover your dream job with NexHire. Connect with top
+                employers, explore <br /> exciting opportunities, and take the
+                next step in your career journey.
               </p>
               <button className="btn btn-primary">Get Started</button>
             </div>
-            <div className="w-[50%]">
-              <Lottie animationData={bannerLottie}></Lottie>
+            <div className="w-[40%]">
+              <Lottie
+                className="w-[600px]"
+                animationData={bannerLottie}
+              ></Lottie>
             </div>
           </div>
         </div>
