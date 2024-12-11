18b9bc37b05182943bdbdf28b8a697b58116cad5
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-11 11:02:59 +0600

    added react typewritter animation in title in banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,17 +1,23 @@
 import Lottie from "lottie-react";
 import { motion } from "motion/react";
 import bannerLottie from "../../../public/lottieFiles/bannerLottie.json";
+import { Typewriter } from "react-simple-typewriter";
 const Banner = () => {
   return (
     <div className="mb-72 mt-12 bg-purple-50">
       <div className="hero h-[480px]">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="flex items-center ">
-            <div className="w-[60%]">
-              <h1 className="text-5xl font-bold text-purple-950">
-                Your Next <span className="text-purple-600">Opportunity</span>{" "}
-                Awaits!
-              </h1>
+            <div className="w-[60%] text-purple-900 font-extrabold text-5xl">
+              <Typewriter
+                words={["Your Next Opportunity Awaits"]}
+                loop={0}
+                cursor
+                cursorStyle="!"
+                typeSpeed={100}
+                deleteSpeed={200}
+                delaySpeed={2000}
+              />
               <p className="py-6 text-justify text-xl font-semibold text-slate-700">
                 Discover your dream job with NexHire. Connect with top
                 employers, explore <br /> exciting opportunities, and take the
