bf6e73e3732a4bfdba193c20980ef5615f78eb79
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-12 20:42:42 +0600

    loaded all jobs data from mongodb

--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -1,7 +1,15 @@
+import axios from "axios";
+import { useState } from "react";
+
 const AllJobs = () => {
+  const [jobs, setJobs] = useState([]);
+
+  axios.get("http://localhost:5000/allJobs").then((data) => setJobs(data.data));
   return (
-    <div>
-      <h1> AllJobs</h1>
+    <div className="mt-24 mb-72">
+      <h1 className="text-center font-bold text-5xl">
+        Total Jobs : {jobs.length}{" "}
+      </h1>
     </div>
   );
 };
