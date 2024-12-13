bcdcfe43c979f5cdaa4a54ab9b2dfea1582d42ea
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-13 22:42:14 +0600

    added toogling post button and showing conditionally 3 to 7 post using slice

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -2,14 +2,20 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import BlogsCard from "../BlogsCard/BlogsCard";
 import { GiMeepleCircle } from "react-icons/gi";
+import { MdUnfoldLess } from "react-icons/md";
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
+  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/blogs")
       .then((data) => setBlogs(data.data));
   }, []);
+
+  const handleLoadMorePost = () => {
+    setShowAll((state) => !state);
+  };
 
   return (
     <div className="mt-24 mb-24">
@@ -21,13 +27,25 @@ const Blogs = () => {
       </p>
 
       <div className="grid grid-cols-3 gap-y-10 mt-16">
-        {blogs.map((blog) => (
+        {blogs.slice(0, showAll ? blogs.length : 3).map((blog) => (
           <BlogsCard key={blog._id} blog={blog}></BlogsCard>
         ))}
       </div>
-      <button className="mt-12 border p-2 w-44 rounded-xl ml-[720px] bg-purple-950 text-white font-bold text-lg flex items-center gap-1">
-        <GiMeepleCircle className="mt-0.5" />
-        Load More Post
+      <button
+        onClick={handleLoadMorePost}
+        className="mt-12 border p-2 w-44 rounded-xl ml-[720px] bg-purple-950 text-white font-bold text-lg flex items-center gap-1"
+      >
+        {showAll ? (
+          <>
+            <MdUnfoldLess className="mt-0.5" />
+            Show Less Post
+          </>
+        ) : (
+          <>
+            <GiMeepleCircle className="mt-0.5" />
+            Load More Post
+          </>
+        )}
       </button>
     </div>
   );
