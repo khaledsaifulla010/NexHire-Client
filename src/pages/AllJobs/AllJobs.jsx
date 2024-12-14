32a21a8662620721ef7ed9a872604e023e149a79
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-14 12:42:23 +0600

    added stylish two images in All Jobs pages

--- src/pages/AllJobs/AllJobs.jsx
+++ src/pages/AllJobs/AllJobs.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AllJobsCard from "../AllJobsCard/AllJobsCard";
+import jobs1 from "../../../public/assets/allJobs/jobs1.jpg";
+import jobs2 from "../../../public/assets/allJobs/jobs2.jpg";
 
 const AllJobs = () => {
   const [jobs, setJobs] = useState([]);
@@ -12,13 +14,20 @@ const AllJobs = () => {
   }, []);
   return (
     <div className="mt-16 mb-72">
-      <div className="w-full h-[300px] border-2 bg-purple-50 rounded-xl">
-        <h1 className="text-center font-bold text-purple-950 text-3xl py-8">
+      <div className="w-full h-[400px] border border-purple-100 bg-purple-50 rounded-xl">
+        <h1 className="text-center font-bold text-purple-950 text-4xl py-10">
           <span className="text-purple-700 font-extrabold">
             {jobs.length} Jobs
           </span>{" "}
           Available Now
         </h1>
+        <p className="text-center text-xl font-bold text-slate-400 -mt-8">
+          Discover your dream job with NexHire.
+        </p>
+        <div className="flex items-center justify-between">
+          <img className="w-[250px] h-[250px]   rounded-xl" src={jobs1} />
+          <img className="w-[250px] h-[250px]  rounded-xl" src={jobs2} />
+        </div>
       </div>
       <div className="grid grid-cols-3 gap-y-12 mt-16 justify-center ml-8">
         {jobs.map((job) => (
