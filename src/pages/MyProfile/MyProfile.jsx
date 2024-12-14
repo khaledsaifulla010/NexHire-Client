3f9727f43ffa5bd3cd626c72b8a3d8b1c2e4f13e
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-12-14 20:29:51 +0600

    added about me section in my profile

--- src/pages/MyProfile/MyProfile.jsx
+++ src/pages/MyProfile/MyProfile.jsx
@@ -13,7 +13,8 @@ const MyProfile = () => {
           Add A Cover Photo <FaCamera />
         </a>
       </div>
-      <div className="mt-12 w-full border rounded-xl h-96 p-2 bg-base-200">
+      <div className="mt-12 w-full border rounded-xl p-2 bg-base-200">
+        {/* div1 */}
         <div className="flex items-center gap-8">
           <img className=" w-[250px] h-[250px] rounded-3xl" src={image1} />
           <div>
@@ -29,6 +30,19 @@ const MyProfile = () => {
             className="w-[100px] h-[200px] absolute right-36 -bottom-8"
             src={bookmark}
           />
+        </div>
+        {/* div2 */}
+
+        <div className="mt-12">
+          <div className=" rounded-xl w-[700px] h-[300px] p-4 bg-white">
+            <h1 className="font-bold text-xl ml-2 ">About Me</h1>
+            <p className="mt-4 font-medium text-slate-500 text-justify px-2">
+              This appears to be a stylized bookmark icon with red, white, and
+              blue stripes. It is often used to represent saving, marking, or
+              highlighting something digitally, such as in a reading app or a
+              document. If you need further clarification or edits, let me know!
+            </p>
+          </div>
         </div>
       </div>
     </div>
